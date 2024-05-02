4008f228f41e6cd0c5b4896ee792a7bf57c074b5
Artz.Fxz <felipepolegatofr@gmail.com>
2024-05-02 17:14:49 -0300

    feat: Ex1

---
+++ ex1.js
@@ -0,0 +1,13 @@
+//Exercício 1: Verificação de login
+//Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
+//nome de usuário é &quot;admin&quot; e a senha é &quot;senha123&quot;. Exiba uma mensagem indicando se o login
+//foi bem-sucedido ou não.
+
+let username = prompt("Digite um nome de usuario: ")
+let senha = prompt("Digite a senha: ")
+
+if (username == "admin") {
+
+}
+
+console.log(1 == "1")
