c9a991075dfe54e2b4679433a43dee65406a7607
Artz.Fxz <felipepolegatofr@gmail.com>
2024-05-08 15:37:46 -0300

    feat

---
+++ ex10loop.js
@@ -0,0 +1,12 @@
+// WHILE
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+let num = 1
+
+while (num != 0) {
+    num = parseInt(prompt("Digite um número:"))
+
+}
+
+console.log("Você conseguiu!")
