2aa4402e6b9370e706f7a6ab27b095239b99011a
Artz.Fxz <felipepolegatofr@gmail.com>
2024-05-08 15:50:01 -0300

    feat: Ajustes Exs

--- ex4loop.js
+++ ex4loop.js
@@ -2,6 +2,17 @@
 // 4. Faça um programa que imprima na tela a tabuada de um número
 // qualquer escolhido pelo usuário até o 10.
 
-for (let i = 0; i <= 90; i += 9) {
-    console.log(i)
+let num = parseInt(prompt("Informe um número de 1 a 10"))
+
+if (num >= 1 && num <= 10) {
+    console.log("Tabuada do " + num + ":\n")
+    for (i = 1; i <= 10; i++) {
+        r = num * i
+        console.log(num + "x" + i + " = " + r)
+
+    }
+}
+
+else {
+    alert("é necessário informar um número de 1 a 10!")
 }
