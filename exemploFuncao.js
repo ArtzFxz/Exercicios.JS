3277e57f49a7f5e32c06eb0a3350dbb7767720f0
Artz.Fxz <felipepolegatofr@gmail.com>
2024-05-09 16:48:36 -0300

    feat: Exemplos Função e Objeto

---
+++ exemploFuncao.js
@@ -0,0 +1,31 @@
+// Chamando a função alert
+// alert("Hello World")
+
+// Chamando o método log()
+// console.log("Olá Felipe!")
+
+// Declaração da função - Ensinando o computador
+// function bemVindo(nome) {
+//     console.log("Bem vindo " + nome);
+// }
+
+// let retorno = bemVindo()
+// bemVindo("Felipe")
+
+
+// function somar(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+
+// function mostrarSituacaoAluno() {
+//     let nota1 = parseFloat(prompt("Digite a primeira nota: "))
+//     let nota2 = parseFloat(prompt("Digite a segunda nota: "))
+//     let nota3 = parseFloat(prompt("Digite a terceira nota: "))
+
+//     let somaNotas = nota1 + nota2 + nota3
+
+//     let media = somaNotas / 3
+
+//     console.log(media)
+// }
---
+++ exemploObjeto.js
@@ -0,0 +1,49 @@
+// NOME
+// let nome = "Felipe"
+// let nome1 = "Thiago"
+
+// APELIDO
+// let apelido = "Polegato"
+// let apelido1 = "Thigas"
+
+
+
+let pessoa1 = {
+    nome: "Felipe",
+    apelido: "Polegato"
+}
+
+let pessoa2 = {
+    nome: "Thiago",
+    apelido: "Thigas"
+}
+
+
+let produto = {
+    nomeProduto: "Tônico Capilar",
+    marca: "Manual",
+    descricao: "Acabe com a calvice",
+    preco: 29.90
+}
+
+// Concatenação
+console.log(produto.preco + " " + produto.descricao);
+
+console.log("Olá Thiago, acabe com a calvice, utilizando a Manual")
+
+
+// Interpolação ( Template Strings )
+console.log(`Olá ${pessoa1.nome}, ${produto.descricao}, utilizando a ${produto.marca}`)
+
+
+let produtoCadastrado = {
+    nome: "",
+    valor: "",
+    marca: ""
+}
+
+produtoCadastrado.nome = prompt("Digite o nome do produto: ")
+produtoCadastrado.valor = parseFloat(prompt("Digite o valor do produto: "))
+produtoCadastrado.marca = prompt("Digite a marca do produto: ")
+
+console.log(produtoCadastrado)
